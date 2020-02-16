24890a32c1bcbd46847f5bdfc3b45bac25ade88f
AJ Carter <ajcarter@0587353175.wireless.umich.net>
2020-02-16 02:14:10 -0500

    added js for scroll top

---
+++ js/uxe.js
@@ -0,0 +1,22 @@
+topbutton = document.getElementById('top_button')
+
+window.onscroll = function() {
+    ScrollFunction()
+};
+
+
+function ScrollFunction() {
+
+    if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+        document.getElementById('top_button').style.display = "block";
+    } else {
+        document.getElementById('top_button').style.display = "none";
+    }
+
+}
+
+
+function go_to_topFunction() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0;
+}
